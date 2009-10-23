ffe524112488a53a6dfea453fb8fd4c105b812a1
Alexx2000 <Alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2009-10-23 16:16:21 +0000

    FIX: Index out of bounds in options dialog
    git-svn-id: https://svn.code.sf.net/p/doublecmd/code@1729 10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7

--- branches/0.4.5/src/foptions.pas
+++ branches/0.4.5/src/foptions.pas
@@ -1087,6 +1087,7 @@ var
   iPluginIndex: Integer;
 begin
   iPluginIndex:= stgPlugins.Row - 1;
+  if iPluginIndex < 0 then Exit;
   if pcPluginsTypes.ActivePage.Name = 'tsDSX' then
     ptPluginType:= ptDSX
   else if pcPluginsTypes.ActivePage.Name = 'tsWCX' then
