485d44dc785cdc162a4fd51e00f5fe4a79a698f3
cobines <cobines@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2010-01-14 21:11:56 +0000

    FIX: Delete to trash with file paths containing spaces. Fix reading error from gvfs-trash.
    git-svn-id: https://svn.code.sf.net/p/doublecmd/code@2179 10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7

--- branches/0.4.5/src/platform/uOSUtils.pas
+++ branches/0.4.5/src/platform/uOSUtils.pas
@@ -1489,11 +1489,16 @@ end;
 var f: textfile;
     s: string;
 begin
-  popen(f, _PATH_GVFS_TRASH + #32 + FileName, 'r');     // try to delete.
-  readln(f,s);
-  If (Pos('Error trashing',s) = 1) then Result := false // 17.05.2009 - return result from gvfs-trash;
-  else Result := true;
-  pclose(f);
+  // Open pipe to gvfs-trash to read the output in case of error.
+  // The errors are written to stderr hence "2>&1" is needed because popen only catches stdout.
+  if popen(f, _PATH_GVFS_TRASH + #32 + QuoteStr(FileName) + ' 2>&1', 'r') = 0 then
+  begin
+    readln(f,s);
+    Result := not (Pos('Error trashing',s) = 1);
+    pclose(f);
+  end
+  else
+    Result := False;
 end;
 {$ENDIF}
 // --------------------------------------------------------------------------------
@@ -1968,4 +1973,4 @@ finalization
 
 {$ENDIF}
 
-end.
+end.
