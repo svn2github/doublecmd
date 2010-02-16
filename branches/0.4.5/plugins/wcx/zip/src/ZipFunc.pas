d17171c7f65d4859eb22efff9a1db3a19904bc0b
Alexx2000 <Alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2010-02-16 22:18:41 +0000

    FIX: Small fix
    git-svn-id: https://svn.code.sf.net/p/doublecmd/code@2456 10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7

--- branches/0.4.5/plugins/wcx/zip/src/ZipFunc.pas
+++ branches/0.4.5/plugins/wcx/zip/src/ZipFunc.pas
@@ -300,7 +300,7 @@ procedure SetProcessDataProc (hArcData : TArcHandle; pProcessDataProc1 : TProces
 var
  Arc : TAbZipKitEx;
 begin
-  if (hArcData <> wcxInvalidHandle) then  // if archive is open
+  if (hArcData <> INVALID_HANDLE_VALUE) then  // if archive is open
    begin
      Arc := TAbZipKitEx(Pointer(hArcData));
      if Assigned(pProcessDataProc1) then
