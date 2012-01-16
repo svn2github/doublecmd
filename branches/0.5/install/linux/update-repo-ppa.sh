ad6cbf68b9ff39fa8c600b6e8ddd74fdd664945b
alexx2000 <alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2012-01-16 16:04:21 +0000

    UPD: Version number
    git-svn-id: https://svn.code.sf.net/p/doublecmd/code@4263 10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7

--- branches/0.5/install/linux/update-repo-ppa.sh
+++ branches/0.5/install/linux/update-repo-ppa.sh
@@ -3,7 +3,7 @@
 # This script updates Double Commander Personal Package Archive (PPA) repository
 
 # Set Double Commander version
-DC_VER=0.5.3
+DC_VER=0.5.3.1
 # Set Ubuntu series
 DISTRO=( lucid maverick natty oneiric )
 
