c308b9a1ebf12e766dbd97bd30a21f5840395787
alexx2000 <alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2016-02-21 21:14:11 +0000

    UPD: Merged revision(s) 6608-6614 from trunk
    git-svn-id: https://svn.code.sf.net/p/doublecmd/code@6615 10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7

--- branches/0.7/src/platform/upixmapmanager.pas
+++ branches/0.7/src/platform/upixmapmanager.pas
@@ -49,7 +49,7 @@ uses
   uFile, uIconTheme, uDrive, uDisplayFile, uGlobs, uDCReadPSD
   {$IF DEFINED(UNIX)}
     {$IF NOT DEFINED(DARWIN)}
-    , contnrs, uDCReadSVG
+    , contnrs, uDCReadSVG, uGio, uOSUtils
       {$IFDEF LCLGTK2}
       , gtk2
       {$ELSE}
@@ -129,6 +129,7 @@ type
     {$ELSE}
     FIconTheme: TIconTheme;
     {$ENDIF}
+    FHomeFolder: String;
     {$ENDIF}
     {en
        Maps theme icon name to index of bitmap (in FPixmapList) for this icon.
@@ -1183,6 +1184,7 @@ begin
   if not FUseSystemTheme then FUseSystemTheme:= not (systemVersion < $1060);
   {$ELSEIF DEFINED(UNIX)}
   FExtToMimeIconName := TFPDataHashTable.Create;
+  FHomeFolder := IncludeTrailingBackslash(GetHomeDir);
   {$ENDIF}
 
   FThemePixmapsFileNames := TStringHashList.Create(True);
@@ -1655,8 +1657,9 @@ begin
          (not (DirectAccess and (IsSysFile or FileIsReadOnly(Attributes)) and mbFileExists(FullPath + '\desktop.ini'))) or
          (GetDeviceCaps(Application.MainForm.Canvas.Handle, BITSPIXEL) < 16) then
       {$ELSEIF DEFINED(UNIX) AND NOT DEFINED(DARWIN)}
-      if (IconsMode = sim_all_and_exe) and
-         (DirectAccess and mbFileAccess(Path + Name + '/.directory', fmOpenRead)) then
+      if (IconsMode = sim_all_and_exe) and (DirectAccess) then
+      begin
+        if mbFileAccess(Path + Name + '/.directory', fmOpenRead) then
         begin
           if LoadIcon then
             Result := GetIconByDesktopFile(Path + Name + '/.directory', FiDirIconID)
@@ -1664,6 +1667,16 @@ begin
             Result := -1;
           Exit;
         end
+        else if (FHomeFolder = Path) then
+        begin
+          if LoadIcon then
+            Result := CheckAddThemePixmap(GioFileGetIcon(FullPath))
+          else
+            Result := -1;
+          Exit;
+        end
+        else Exit(FiDirIconID);
+      end
       else
       {$ELSEIF DEFINED(DARWIN)}
       if (IconsMode = sim_all_and_exe) and
