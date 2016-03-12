d426c0a44260ab6e1addb8ab9dc619f9fabb0556
alexx2000 <alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2016-03-12 21:51:57 +0000

    UPD: Ubuntu distro list
    git-svn-id: https://svn.code.sf.net/p/doublecmd/code@6691 10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7

--- branches/0.7/install/linux/update-repo-ppa.sh
+++ branches/0.7/install/linux/update-repo-ppa.sh
@@ -5,7 +5,7 @@
 # Set Double Commander version
 DC_VER=0.7.0
 # Set Ubuntu series
-DISTRO=( trusty vivid wily )
+DISTRO=( wily xenial )
 
 # Temp directory
 DC_TEMP_DIR=/var/tmp/doublecmd-$(date +%y.%m.%d)
