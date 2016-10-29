f486c1ed12cba2e7dd4fa3bd3fd9d8032b3f4227
alexx2000 <alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2016-10-29 08:15:04 +0000

    UPD: Version number
    git-svn-id: https://svn.code.sf.net/p/doublecmd/code@7172 10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7

--- branches/0.7/install/linux/update-repo-ppa.sh
+++ branches/0.7/install/linux/update-repo-ppa.sh
@@ -3,7 +3,7 @@
 # This script updates Double Commander Personal Package Archive (PPA) repository
 
 # Set Double Commander version
-DC_VER=0.7.5
+DC_VER=0.7.6
 # Set Ubuntu series
 DISTRO=( wily xenial )
 
