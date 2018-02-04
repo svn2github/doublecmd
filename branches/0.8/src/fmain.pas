48896b52e4fc8c141b00d002cac52b4a6d0882db
alexx2000 <alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2018-02-04 11:56:46 +0000

    UPD: Merge fixes from trunk (7951,7952,7955,7956)
    git-svn-id: https://svn.code.sf.net/p/doublecmd/code@7989 10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7

--- branches/0.8/src/fmain.pas
+++ branches/0.8/src/fmain.pas
@@ -5800,6 +5800,7 @@ const
   PTLen = 40;
 var
   st: String;
+  Properties: TFileSourceProperties;
 begin
   if (fsoExecute in ActiveFrame.FileSource.GetOperationsTypes) then
   begin
@@ -5835,7 +5836,8 @@ begin
     edtCommand.Visible := False;
   end;
   // Change program current path
-  if (fspDirectAccess in ActiveFrame.FileSource.GetProperties) then
+  Properties := ActiveFrame.FileSource.GetProperties;
+  if (fspDirectAccess in Properties) and not (fspLinksToLocalFiles in Properties) then
   begin
     mbSetCurrentDir(ActiveFrame.CurrentPath);
   end;
