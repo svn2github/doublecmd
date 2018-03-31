a0a016d8934e241b1ccc9a7e0dbf7f1b1f334cdf
alexx2000 <alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2018-03-31 09:49:41 +0000

    UPD: Merged revision(s) 7982, 8043 from trunk
    git-svn-id: https://svn.code.sf.net/p/doublecmd/code@8045 10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7

--- branches/0.8/src/fmain.pas
+++ branches/0.8/src/fmain.pas
@@ -3629,7 +3629,7 @@ begin
   begin
     DisableAutoSizing;
     try
-      ScaleFontsPPI(AYProportion);
+      // ScaleFontsPPI(AYProportion);
       BorderSpacing.AutoAdjustLayout(AXProportion, AYProportion);
       Constraints.AutoAdjustLayout(AXProportion, AYProportion);
     finally
