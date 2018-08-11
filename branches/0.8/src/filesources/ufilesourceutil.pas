095886bee3ef280cc66afbc5a8fb43ca5953cd29
alexx2000 <alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2018-08-11 09:04:59 +0000

    UPD: Merged revision(s) 8251, 8253 from trunk
    git-svn-id: https://svn.code.sf.net/p/doublecmd/code@8259 10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7

--- branches/0.8/src/filesources/ufilesourceutil.pas
+++ branches/0.8/src/filesources/ufilesourceutil.pas
@@ -24,6 +24,8 @@ procedure ChooseFile(aFileView: TFileView; aFileSource: IFileSource; aFile: TFil
 }
 function ChooseFileSource(aFileView: TFileView; aFileSource: IFileSource; aFile: TFile): Boolean; overload;
 
+function ParseFileSource(var aPath: String; const CurrentFileSource: IFileSource = nil): IFileSource;
+
 function ChooseFileSource(aFileView: TFileView; const aPath: String; bLocal: Boolean = False): Boolean; overload;
 
 function ChooseArchive(aFileView: TFileView; aFileSource: IFileSource; aFile: TFile; bForce: Boolean = False): Boolean;
@@ -67,14 +69,23 @@ begin
   // For now work only for local files.
   if aFileView.FileSource.Properties * [fspDirectAccess, fspLinksToLocalFiles] <> [] then
   begin
-    if fspLinksToLocalFiles in aFileView.FileSource.Properties then
-      aFileView.FileSource.GetLocalName(aFile);
-
     // Now test if exists Open command in "extassoc.xml" :)
     if gExts.GetExtActionCmd(aFile, 'open', sCmd, sParams, sStartPath) then
     begin
-      if ProcessExtCommandFork(sCmd,sParams,sStartPath) then
-        Exit;
+      try
+        // Resolve filename here since ProcessExtCommandFork doesn't do it (as of 2017)
+        // The limitation is that only one file will be opened on a FileSource of links
+        if fspLinksToLocalFiles in aFileView.FileSource.Properties then
+        begin
+          aFileCopy := aFile.Clone;
+          aFileView.FileSource.GetLocalName(aFileCopy);
+        end;
+
+        if ProcessExtCommandFork(sCmd,sParams,sStartPath,aFileCopy) then
+          Exit;
+      finally
+        FreeAndNil(aFileCopy);
+      end;
     end;
   end;
 
@@ -178,15 +189,11 @@ begin
   end;
 end;
 
-function ChooseFileSource(aFileView: TFileView; const aPath: String;
-  bLocal: Boolean): Boolean;
+function ParseFileSource(var aPath: String; const CurrentFileSource: IFileSource = nil): IFileSource;
 var
   URI: TURI;
-  RemotePath: String;
-  FileSource: IFileSource;
   aFileSourceClass: TFileSourceClass;
 begin
-  Result:= True;
   aFileSourceClass:= gVfsModuleList.GetFileSource(aPath);
   // If found special FileSource for path
   if Assigned(aFileSourceClass) then
@@ -195,25 +202,52 @@ begin
       if Pos('://', aPath) > 0 then
         begin
           URI:= ParseURI(aPath);
-          RemotePath:= NormalizePathDelimiters(URI.Path + URI.Document);
-          RemotePath:= IncludeTrailingPathDelimiter(RemotePath);
-          FileSource:= FileSourceManager.Find(aFileSourceClass,
+          aPath:= NormalizePathDelimiters(URI.Path + URI.Document);
+          aPath:= IncludeTrailingPathDelimiter(aPath);
+          Result:= FileSourceManager.Find(aFileSourceClass,
                                               URI.Protocol + '://' + URI.Host,
                                               not SameText(URI.Protocol, 'smb')
                                               );
-          if Assigned(FileSource) then
-            aFileView.AddFileSource(FileSource, RemotePath)
-          else begin
+          if not Assigned(Result) then
+          try
             // Create new FileSource with given URI
-            aFileView.AddFileSource(aFileSourceClass.Create(URI), RemotePath);
+            Result := aFileSourceClass.Create(URI);
+          except
+            Result := nil;
           end;
         end
       // If found FileSource is same as current then simply change path
-      else if aFileSourceClass.ClassNameIs(aFileView.FileSource.ClassName) then
+      else if aFileSourceClass.ClassNameIs(CurrentFileSource.ClassName) then
+        Result := CurrentFileSource
+      // Else create new FileSource with given path
+      else
+        Result := aFileSourceClass.Create;
+    end
+  else
+    Result:= nil;
+end;
+
+function ChooseFileSource(aFileView: TFileView; const aPath: String;
+  bLocal: Boolean): Boolean;
+var
+  RemotePath: String;
+  FileSource: IFileSource;
+begin
+  Result:= True;
+  RemotePath:= aPath;
+  FileSource:= ParseFileSource(RemotePath, aFileView.FileSource);
+  // If found special FileSource for path
+  if Assigned(FileSource) then
+    begin
+      // If path is URI
+      if RemotePath <> aPath then
+        aFileView.AddFileSource(FileSource, RemotePath)
+      // If found FileSource is same as current then simply change path
+      else if aFileView.FileSource.Equals(FileSource) then
         aFileView.CurrentPath := aPath
       // Else create new FileSource with given path
       else
-        aFileView.AddFileSource(aFileSourceClass.Create, aPath);
+        aFileView.AddFileSource(FileSource, aPath);
     end
   // If current FileSource has address
   else if bLocal and (Length(aFileView.CurrentAddress) > 0) then
